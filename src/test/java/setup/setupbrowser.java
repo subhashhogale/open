88d44f57cadb5ea7cee386c474df84d74603f2d7
subhash124 <subhashhogale5@gmail.com>
2025-03-26 07:06:00 +0530

    testng

--- src/test/java/setup/setupbrowser.java
+++ src/test/java/setup/setupbrowser.java
@@ -2,15 +2,17 @@ package setup;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class setupbrowser {
 
-	public static void main(String[] args) {
-		
+	
+		@Test
+		public void strat() {
 		WebDriver driver=new ChromeDriver();
 		
 		driver.get("https://www.youtube.com/watch?v=qR20KF7wxSU");;
-
-	}
+//
+		}
 
 }
